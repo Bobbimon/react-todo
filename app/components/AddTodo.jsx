0c44e4bbb98d0dbfa5ffe1bda4528bf660f812b1
bobbimon <tobbe_lobbe88@hotmail.com>
2017-03-26 14:21:13 +0200

    Changing onSubmit to handleSubmit

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -1,20 +1,20 @@
 var React = require('react');
 
 var TodoForm = React.createClass({
-  onSubmit: function (e) {
+  handleSubmit: function (e) {
     e.preventDefault();
-    var todo = this.refs.todo.value;
+    var todo = this.refs.todoText.value;
     if (todo.length > 1) {
-      this.refs.todo.value = '';
+      this.refs.todoText.value = '';
       this.props.onAddTodo(todo);
     }
   },
   render: function () {
     return (
       <div>
-        <form ref="form" onSubmit={this.onSubmit} className="todo-form">
-          <input type="text" ref="todo" placeholder="Add todo"/>
-          <button className="button expanded">Add</button>
+        <form ref="form" onSubmit={this.handleSubmit} className="todo-form">
+          <input type="text" ref="todoText" placeholder="Add todo"/>
+          <button className="button expanded">Add Todo</button>
         </form>
       </div>
     );
