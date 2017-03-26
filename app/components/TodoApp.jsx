0e0ebd39e916676951cde667d153485cbfc25106
bobbimon <tobbe_lobbe88@hotmail.com>
2017-03-26 14:17:45 +0200

    Refactored TodoForm to AddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');
-var TodoForm = require('TodoForm');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -23,7 +23,7 @@ var TodoApp = React.createClass({
     };
   },
   handleAddTodo: function (text) {
-    alert('new todo ' + text);
+    alert('new todo: ' + text);
   },
   render: function () {
     var {todos} = this.state;
@@ -31,7 +31,7 @@ var TodoApp = React.createClass({
       <div className="row">
         <div className="columns medium-6 large-4 small-centered">
           <TodoList todos={todos}/>
-          <TodoForm onAddTodo={this.handleAddTodo}/>
+          <AddTodo onAddTodo={this.handleAddTodo}/>
         </div>
       </div>
     );
--- app/tests/components/TodoForm.test.jsx
+++ app/tests/components/TodoForm.test.jsx
@@ -4,29 +4,29 @@ var Expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
-var TodoForm = require('TodoForm');
+var AddTodo = require('AddTodo');
 
-describe('TodoForm', () => {
+describe('AddTodo', () => {
   it('Should exist', ()=> {
-    Expect(TodoForm).toExist();
+    Expect(AddTodo).toExist();
   });
 
   it('Should call onAddTodo if valid todo was entered', () => {
     var spy = Expect.createSpy();
-    var todoForm = TestUtils.renderIntoDocument(<TodoForm onAddTodo={spy}/>);
-    var $el = $(ReactDOM.findDOMNode(todoForm));
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
 
-    todoForm.refs.todo.value = 'Do the dishes';
+    addTodo.refs.todo.value = 'Do the dishes';
     TestUtils.Simulate.submit($el.find('form')[0]);
     Expect(spy).toHaveBeenCalledWith('Do the dishes');
   });
 
   it('Should not call onAddTodo if invalid todo was entered', () => {
     var spy = Expect.createSpy();
-    var todoForm = TestUtils.renderIntoDocument(<TodoForm onAddTodo={spy}/>);
-    var $el = $(ReactDOM.findDOMNode(todoForm));
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
 
-    todoForm.refs.todo.value = '';
+    addTodo.refs.todo.value = '';
     TestUtils.Simulate.submit($el.find('form')[0]);
     Expect(spy).toNotHaveBeenCalled();
   });
