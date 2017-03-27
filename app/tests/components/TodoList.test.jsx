543853b225e90f32bff8aaa01c5c2af76fa42cae
bobbimon <tobbe_lobbe88@hotmail.com>
2017-03-27 21:04:02 +0200

    Style todo app

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -7,12 +7,12 @@ var TestUtils = require('react-addons-test-utils');
 var TodoList = require('TodoList');
 var Todo = require('Todo');
 
-describe('TodoList', () => {
-  it('Should exist', () => {
+describe ('TodoList', () => {
+  it ('Should exist', () => {
     Expect(TodoList).toExist();
   });
 
-  it('Should render one Todo component for each todo item', () => {
+  it ('Should render one Todo component for each todo item', () => {
     var todos = [{
       id: 1,
       text: 'Do something'
@@ -24,5 +24,13 @@ describe('TodoList', () => {
     var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo)
 
     Expect(todosComponents.length).toBe(todos.length);
+  });
+
+  it ('Should render empty message if no todos', () => {
+    var todos = [];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var $el = $(ReactDOM.findDOMNode(todoList));
+
+    Expect($el.find('.container__message').length).toBe(1);
   });
 });
