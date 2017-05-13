dc27ae46e91a6497f65d284df6d28c7bb1691b8c
bobbimon <tobbe_lobbe88@hotmail.com>
2017-05-13 11:57:29 +0200

    Added enviromental values

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-   apiKey: "AIzaSyBquTgHxEmxRD8avbFBLn08O61V2SYhILc",
-   authDomain: "todo-app-ac434.firebaseapp.com",
-   databaseURL: "https://todo-app-ac434.firebaseio.com",
-   storageBucket: "todo-app-ac434.appspot.com",
-   messagingSenderId: "775439300049"
+   apiKey: process.env.API_KEY,
+   authDomain: process.env.AUTH_DOMAIN,
+   databaseURL: process.env.DATABASE_URL,
+   projectId: process.env.PROJECT_ID,
+   storageBucket: process.env.STORAGE_BUCKET,
+   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   };
   firebase.initializeApp(config);
 } catch (e) {
